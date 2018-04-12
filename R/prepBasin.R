63bb138910476e5bf75847234a5af4ee6356af1c
darrennorris <doon75@hotmail.com>
2018-04-12 13:05:09 -0300

    add examples and export shapefiles

--- R/prepBasin.R
+++ R/prepBasin.R
@@ -1,7 +1,10 @@
 #' @title Prepare basin polygons
-#' @description Generates files for subsequent use.
+#' @description Generates files for subsequent use. 
+#' Optionally creates "SpeciesBasin.shp" .
 #'
 #' @param Bain Input file with basin polygons
+#' @param make_shape Logical (TRUE/FALSE). Generate polygon shapefile 
+#' covering basins with species locations ("SpeciesBasin.shp").
 #' @details Takes georeferenced polygons of river basins
 #'
 #' @return Files for subsequent use.
@@ -13,7 +16,7 @@
 #' @examples
 #' \dontrun{
 #' }
-prepBasin <- function(Bain = NA){
+prepBasin <- function(Bain = NA, make_shape = FALSE){
   sf1 <- sf::read_sf(Bain)
   sf1clean <- sf::st_union(sf::st_buffer(sf1,0), by_feature = TRUE) %>% 
     dplyr::group_by(BASIN_NAME, subbasin) %>% 
@@ -33,6 +36,26 @@ prepBasin <- function(Bain = NA){
   sf13395$area_km2 <- units::set_units(sf::st_area(sf13395), km^2)
   sf1clean$area_km2 <- sf13395$area_km2
   sf1clean$arearank <- rank(sf1clean$area_km2) 
+  
+  # make a single polygon covering all basins where species is
+  # buffer needed to dissolve borders, but still has slivers
+  if(make_shape!=FALSE){
+  sf.speciesBasin <- sf::st_union(sf::st_buffer(sf1clean,0.00005))
+  sf.speciesBasin <- st_sf(sf.speciesBasin)
+  sf.speciesBasin$sbid <- "P.unifilis"
+  st_write(sf.speciesBasin, "spciesBasin.shp")
+  
+  #sin <- file.choose()
+  # this file has second feature covering holes and slivers
+  #[1] "C:\\Users\\Darren\\Documents\\ms\\unpublished\\2018 Unifilis demography\\analises\\shapes\\amazon_orinoco\\spciesBasin.shp"
+  #td <- st_read(sin)
+  #sf.td <- sf::st_union(sf::st_buffer(td,0.0))
+  #sf.td <- st_sf(sf.td)
+  #sf.td$sbid <- "P.unifilis"
+  #st_write(sf.td, "speciesBasin.shp")
+  
+  }
+  
   return(sf1clean)
   
 }
--- R/prepPAcover.R
+++ R/prepPAcover.R
@@ -1,11 +1,16 @@
 #' @title Prepare polygons for PAcover calculations
-#' @description Generates files for subsequent use.
+#' @description Generates files for subsequent use. Optionally exports
+#' polygon as shapefile "pacover.shp".
 #'
 #' @param pPA Input file with protected area polygons.
 #' @param pBasin Input file with basin polygons.
-#' @param pBasinSp Input file with polygon covering basins in species range of occurance.
+#' @param pBasinSp Input file with polygon covering basins 
+#' in species range of occurance.
+#' @param make_shape Logical (TRUE/FALSE). Generate polygon shapefile of
+#' protected areas in 3 classes across basins in species range.
 #'
-#' @return Polygons of area covered by protected areas.
+#' @return Polygons of area covered by protected areas. Optionally exports
+#' polygon as shapefile "pacover.shp".
 #' @details To remove overlapping areas with different PA classifications.
 #' These are big areas and processing takes time: 4 - 5 hours on a laptop.
 #' @import sf
@@ -19,6 +24,8 @@
 #' # These large files are not available via github package. 
 #' # Download "shapes.zip" from :
 #' # https://drive.google.com/open?id=1QQArA7pPLemUVQTKx7PxigsQXKpOG0YQ
+#' # For the code below to work, 
+#' # extract files in "shapes.zip" to cmartr/inst/shape .
 #' Bsp <- system.file("shape/speciesBasin.shp", package="cmartr")
 #' PA <- system.file("shape/wdpaselect.shp", package="cmartr")
 #' B <- system.file("shape/amazon_orinoco.shp", package="cmartr")
@@ -26,7 +33,7 @@
 #' sfclean$sbid <- "P.unifilis"
 #' pacover <- prepPAcover(pPA = PA, pBasin = B, pBasinSp = Bsp)
 #' }
-prepPAcover <- function(pPA = NA, pBasin = NA, pBasinSp = NA){
+prepPAcover <- function(pPA = NA, pBasin = NA, pBasinSp = NA, make_shape = FALSE){
   # big spatial processing needs memory
   memory.limit(84000)
   # clean and prep shapefiles
@@ -79,6 +86,10 @@ prepPAcover <- function(pPA = NA, pBasin = NA, pBasinSp = NA){
   # as proportion of total area covered by basins
   totarea <- sum(units::set_units( sf::st_area(sf::st_transform(sfclean, crs=3395)), km^2))
   sf.pa.cropclean$area_prop <- sf.pa.cropclean$area_km2 / totarea
+  
+  if(make_shape!=FALSE){
+    st_write(st_transform(sf.pa.cropclean, crs=4326), "pacover.shp")
+  }
   
   return(sf.pa.cropclean)
   
